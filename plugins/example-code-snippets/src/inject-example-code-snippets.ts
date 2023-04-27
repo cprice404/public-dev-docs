ee679bb174bb64bfc8e154ee847e53ba38f89233
Chris Price <chris@momentohq.com>
2023-04-26 21:14:55 -0700

    WIP builds again

--- plugins/example-code-snippets/src/inject-example-code-snippets.ts
+++ plugins/example-code-snippets/src/inject-example-code-snippets.ts
@@ -1,8 +1,12 @@
-// import visit from 'unist-util-visit';
+import visit from 'unist-util-visit';
 // eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-unsafe-assignment
-const visit = require('unist-util-visit');
+// const visit = require('unist-util-visit');
 // import {Plugin, Optio} from 'unified';
 // import {} from 'mdast';
+// eslint-disable-next-line import/no-unresolved,node/no-missing-import
+import * as unist from 'unist';
+// import {TestFunctionAnything} from 'unist-util-is';
+// import {TestFunction} from 'unist-util-is';
 
 // for this POC, here we define some hard-coded example snippets.  In the real, working version of this plugin,
 // there would be some code to pull this stuff from the sdk git repos.
@@ -21,9 +25,22 @@ const temporaryHardCodedCodeSnippets = new Map([
   ],
 ]);
 
-function markdownNodeContainsExampleSnippets(node: any) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+/*
+
+  type TestFunction<T extends Node> = (
+    node: unknown,
+    index?: number,
+    parent?: Parent
+  ) => node is T
+ */
+
+function poop<T extends unist.Node>(node: unknown): node is T {
+  // function markdownNodeContainsExampleSnippets(node: unist.Node) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/ban-ts-comment
+  // @ts-ignore
   if (node.type === 'text' || node.type === 'code') {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/ban-ts-comment
+    // @ts-ignore
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
     if (node.value.includes('%%%example:')) {
       return true;
@@ -32,18 +49,33 @@ function markdownNodeContainsExampleSnippets(node: any) {
   return false;
 }
 
+// const foo: (node: unknown) => node is unist.Node = (node: unknown) => {
+//   // function markdownNodeContainsExampleSnippets(node: unist.Node) {
+//   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/ban-ts-comment
+//   // @ts-ignore
+//   if (node.type === 'text' || node.type === 'code') {
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/ban-ts-comment
+//     // @ts-ignore
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+//     if (node.value.includes('%%%example:')) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
 function plugin(options: any): any {
-  console.log(`LOADING EXAMPLE SNIPPETS PLUGIN!`);
-  function transformer(ast: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-call
-    visit(ast, markdownNodeContainsExampleSnippets, node => {
+  console.log('LOADING EXAMPLE SNIPPETS PLUGIN!');
+  function transformer(tree: unist.Node) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    visit(tree, poop, node => {
       console.log(
         'Found a markdown node that contains example snippets.  Replacing content.'
       );
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call,@typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
       node.value = node.value.replace(
         /%%%([^%]*)%%%/g,
         (match: any, exampleId: any) => {
